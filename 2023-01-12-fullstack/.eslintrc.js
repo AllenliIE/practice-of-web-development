e119c2aaedcca4ed184ec67ee98f4a4dc5367045
李政東 <lizhengdong@lizhengdongdeMacBook-Pro.local>
2023-01-12 23:57:10 +0800

    feat: create project

---
+++ 2023-01-12-fullstack/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/vue3-essential"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+        "no-unused-vars": 0
+    }
+}
