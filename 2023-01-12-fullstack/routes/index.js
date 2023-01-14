232973806d80163c8d1c025e74bf875f4e317677
AllenliIE <allen.li.service@gmail.com>
2023-01-14 09:32:11 +0800

    feat: add user signup

--- 2023-01-12-fullstack/routes/index.js
+++ 2023-01-12-fullstack/routes/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const projectController = require("../controllers/project-controller");
 const admin = require("./modules/admin");
+const projectController = require("../controllers/project-controller");
+const userController = require('../controllers/user-controller')
 
 router.use("/admin", admin);
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get("/projects", projectController.getProjects);
 router.use("/", (req, res) => res.redirect("/projects"));
 
