d6e05349ae52ff9bca1e1c0fab4ed7c52f381714
AllenliIE <allen.li.service@gmail.com>
2023-01-16 16:46:21 +0800

    feat: flash msg & signup verification

--- 2023-01-12-fullstack/controllers/user-controller.js
+++ 2023-01-12-fullstack/controllers/user-controller.js
@@ -6,19 +6,27 @@ const userController = {
   signUpPage: (req, res) => {
     res.render("signup");
   },
-  signUp: (req, res) => {
-    bcrypt
-      .hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body;
+    if (password !== passwordCheck) throw new Error("輸入的密碼兩者不相同！");
+
+    User.findOne({ where: { email } })
+      .then((user) => {
+        if (user) throw new Error("此信箱已存在！");
+        return bcrypt.hash(password, 10);
+      })
       .then((hash) =>
         User.create({
-          name: req.body.name,
-          email: req.body.email,
+          name,
+          email,
           password: hash,
         })
       )
       .then(() => {
-        res.redirect("/signin");
-      });
+        req.flash("success_messages", "成功註冊帳號！");
+        req.redirect("/signin");
+      })
+      .catch((err) => next(err));
   },
 };
 
--- 2023-01-12-fullstack/routes/index.js
+++ 2023-01-12-fullstack/routes/index.js
@@ -3,12 +3,15 @@ const router = express.Router();
 
 const admin = require("./modules/admin");
 const projectController = require("../controllers/project-controller");
-const userController = require('../controllers/user-controller')
+const userController = require("../controllers/user-controller");
+
+const { generlErrorHandler } = require("../middleware/error-handler");
 
 router.use("/admin", admin);
-router.get('/signup', userController.signUpPage)
-router.post('/signup', userController.signUp)
+router.get("/signup", userController.signUpPage);
+router.post("/signup", userController.signUp);
 router.get("/projects", projectController.getProjects);
 router.use("/", (req, res) => res.redirect("/projects"));
+router.use("/", generlErrorHandler);
 
 module.exports = router;
